ed07c09ebe497f114cd5cad043591c47599a55e2
Harish1138 <harya0102@gmail.com>
2026-01-07 22:57:13 +0530

    initial commit

--- Array/MOVE_ZERO_TO_END.cpp
+++ Array/MOVE_ZERO_TO_END.cpp
@@ -1,22 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    int count=0;
-    for (int i=0;i<n;i++) {
-        if (arr[i]==0) count++;
-    }
+// Brute solution
+// Option solution available after the main function
+
+// int main() {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//         cin>>arr[i];
+//     int count=0;
+//     for (int i=0;i<n;i++) {
+//         if (arr[i]==0) count++;
+//     }
+//     int j=0;
+//     for (int i=0;i<n;i++) {
+//         if (arr[i]!=0) {
+//             swap(arr[i],arr[j++]);
+//         }
+//     }
+//     for (int k=0;k<n;k++) {
+//         cout<<arr[k]<<" ";
+//     }
+// }
+
+
+// Optimal Solution
+void move_zero(vector<int> &arr) {
     int j=0;
+    int n=arr.size();
+    cout<<"After moved: ";
     for (int i=0;i<n;i++) {
         if (arr[i]!=0) {
             swap(arr[i],arr[j++]);
         }
     }
-    for (int k=0;k<n;k++) {
-        cout<<arr[k]<<" ";
+
+}
+int main() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+
     }
+    cout<<"Before Moved: ";
+    for (int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    move_zero(arr);
+    for (int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+
 }

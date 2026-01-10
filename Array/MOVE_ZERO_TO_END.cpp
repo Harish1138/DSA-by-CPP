69eda7fb6e6a7cda61c1d738440d6f9b35cdae43
Harish1138 <harya0102@gmail.com>
2026-01-10 22:14:21 +0530

    updated

--- Array/MOVE_ZERO_TO_END.cpp
+++ Array/MOVE_ZERO_TO_END.cpp
@@ -27,14 +27,22 @@ using namespace std;
 
 // Optimal Solution
 void move_zero(vector<int> &arr) {
-    int j=0;
+    int j=-1;
     int n=arr.size();
-    cout<<"After moved: ";
     for (int i=0;i<n;i++) {
+        if (arr[i]==0) {
+            j=i;
+            break;
+        }
+
+    }
+    if (j==-1) return;
+    for (int i=j+1;i<n;i++) {
         if (arr[i]!=0) {
             swap(arr[i],arr[j++]);
         }
     }
+
 
 }
 int main() {

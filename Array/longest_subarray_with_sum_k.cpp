2f0f7bbebd3836da15b5d1867cfc039290db55c0
Harish1138 <harya0102@gmail.com>
2026-01-13 10:57:26 +0530

    modified

--- Array/longest_subarray_with_sum_k.cpp
+++ Array/longest_subarray_with_sum_k.cpp
@@ -1,30 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
+// int main() {
+//     int n;
+//     cout<<"Enter Number of Elements: ";
+//     cin>>n;
+//     vector<int> arr(n);
+//     cout<<"Enter Array Elements: ";
+//     for(int i=0;i<n;i++)
+//         cin>>arr[i];
+//     int k;
+//     cout<<"Enter target value";
+//     cin>>k;
+//
+//     int sum=0;
+//     int count=0;
+//     unordered_map<int,int>mpp;
+//     mpp[0]=1;
+//     for (int i=0;i<n;i++) {
+//         sum+=arr[i];
+//         int more=sum-k;
+//         if (mpp.find(more)!=mpp.end()) {
+//             count+=mpp[more];
+//         }
+//         mpp[sum]++;
+//     }
+//     cout<<"Largest Subarray will be: "<<count<<endl;
+//
+//
+// }
 int main() {
     int n;
-    cout<<"Enter Number of Elements: ";
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
-    cout<<"Enter Array Elements: ";
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    int k;
-    cout<<"Enter target value";
-    cin>>k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
 
+    }
+    int k;
+    cin >> k;
     int sum=0;
-    int count=0;
-    unordered_map<int,int>mpp;
-    mpp[0]=1;
+    int maxLen=0;
+    map<int, int> mpp;
     for (int i=0;i<n;i++) {
         sum+=arr[i];
+        if (sum==k) {
+            maxLen=max(maxLen,i+1);
+        }
         int more=sum-k;
         if (mpp.find(more)!=mpp.end()) {
-            count+=mpp[more];
+            int len=i-mpp[more];
+            maxLen=max(maxLen,len);
         }
-        mpp[sum]++;
+        if (mpp.find(sum)==mpp.end()) {
+            mpp[sum]=i;
+        }
+
     }
-    cout<<"Largest Subarray will be: "<<count<<endl;
-
-
+    cout<<maxLen<<endl;
 }
